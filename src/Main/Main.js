863c7927b62e921f68d4904976d55d682a734e41
Alexandr <sashakobzeww@gmail.com>
2019-08-15 20:33:52 +0300

    chr Main Создан и подключён компонент Block

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import './Main.css'
+import Block from './Block/Block'
 
 class Main extends Component {
     render() {
         return (
             <div>
-
+                <Block></Block>
             </div>
         );
     }
