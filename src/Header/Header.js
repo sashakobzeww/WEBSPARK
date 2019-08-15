8ae56ab74f8f2ed0c17f17b3f873ea7542e39e5d
Alexandr <sashakobzeww@gmail.com>
2019-08-15 18:40:02 +0300

    chr Header Основные классы компонента созданы

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,9 +5,16 @@ import logo from "./img/logo.jpg"
 class Header extends Component {
     render() {
         return (
-            <div className="left">
-                <img src={logo} alt="logo"/>
-            </div>
+            <>
+                <div className="left">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="right">
+                    <div className="top"></div>
+                    <div className="info"></div>
+                    <div className="date"></div>
+                </div>
+            </>
         );
     }
 }
