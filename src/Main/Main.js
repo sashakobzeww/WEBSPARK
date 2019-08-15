df377ad224fa1742db4642f14c0929a487218bff
Alexandr <sashakobzeww@gmail.com>
2019-08-15 21:32:05 +0300

    chr Main Добавлен state c одним пока объектом и map

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,11 +3,41 @@ import './Main.css'
 import Block from './Block/Block'
 
 class Main extends Component {
+    state = {
+        blocks: [
+            {
+                fromDay: "Today",
+                fromLikes: "128",
+                fromComments: "31",
+                toDay: "09-08-2016",
+                toLikes: "67",
+                toComments: "22",
+                imageUpload: '11-04-2016',
+
+                img: "./Block/img/block_1.jpg"
+            }
+        ]
+    }
+
+    makeList() {
+        const blocks = this.state.blocks;
+
+        const items = blocks.map((block) =>
+            <>
+                <li>
+                    {block.fromDay}
+                </li>
+            </>
+        )
+
+        return items
+    }
+
     render() {
         return (
-            <div>
-                <Block></Block>
-            </div>
+            <ul>
+                {this.makeList()}
+            </ul>
         );
     }
 }
