28a14a7d007d794773a17e721226285e44dd4eb4
Alexandr <sashakobzeww@gmail.com>
2019-08-15 21:48:47 +0300

    chr Main Содержимое блока наполнено текстом

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -23,11 +23,23 @@ class Main extends Component {
         const blocks = this.state.blocks;
 
         const items = blocks.map((block) =>
-            <>
-                <li>
-                    {block.fromDay}
-                </li>
-            </>
+            <li>
+
+                <div className="from">
+                    <p className="fromDay">{block.fromDay}</p>
+                    <p className="fromLikes"><img src="Block/img/like.png" alt="likes"/>{block.fromLikes}</p>
+                    <p className="fromComments"><img src="Block/img/comment.png" alt="comments"/>{block.fromComments}</p>
+                </div>
+
+                <div className="to">
+                    <p className="toDay">{block.toDay}</p>
+                    <p className="toLikes"><img src="Block/img/like.png" alt="likes"/>{block.toLikes}</p>
+                    <p className="toComments"><img src="Block/img/comment.png" alt="comments"/>{block.toComments}</p>
+                </div>
+
+                <div className="imageUpload">{block.imageUpload}</div>
+
+            </li>
         )
 
         return items
