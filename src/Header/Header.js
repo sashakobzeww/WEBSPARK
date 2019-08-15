08b7137172794e40b1e9bc55e54193b58bffaeaa
Alexandr <sashakobzeww@gmail.com>
2019-08-15 18:13:12 +0300

    chr Header, App Создан и подключён компонент Header

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <p>123</p>
+        );
+    }
+}
+
+export default Header
+
