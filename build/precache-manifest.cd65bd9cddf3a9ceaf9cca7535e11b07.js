8f668a9624a45b5e7d3fac0af1f094d6fb08cc73
Alexandr <sashakobzeww@gmail.com>
2019-08-16 15:37:06 +0300

    Добавлены фалы на продакшн

---
+++ build/precache-manifest.cd65bd9cddf3a9ceaf9cca7535e11b07.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d99bf0f209486aef645b505aabf31f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f19fa012ec265d13963c",
+    "url": "/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "c86f114f9ab719d44f1e",
+    "url": "/static/css/main.71d6df1c.chunk.css"
+  },
+  {
+    "revision": "f19fa012ec265d13963c",
+    "url": "/static/js/2.1741a826.chunk.js"
+  },
+  {
+    "revision": "c86f114f9ab719d44f1e",
+    "url": "/static/js/main.d2ee08b5.chunk.js"
+  },
+  {
+    "revision": "3286612b6bdb5d2e671a",
+    "url": "/static/js/runtime~main.5d7ec425.js"
+  },
+  {
+    "revision": "831aee75d1fd9d9daa95ba7fb7ce1dcb",
+    "url": "/static/media/block_1.831aee75.jpg"
+  },
+  {
+    "revision": "772ed9cb763af7e330eaac5955595460",
+    "url": "/static/media/block_2.772ed9cb.jpg"
+  },
+  {
+    "revision": "dd7d93d45a4b099291a6c3c1ab236853",
+    "url": "/static/media/block_3.dd7d93d4.jpg"
+  },
+  {
+    "revision": "87000b736d1c4a1fec9d5f77a47c6a94",
+    "url": "/static/media/block_4.87000b73.jpg"
+  },
+  {
+    "revision": "c568a00a79acf608b2b76101cbd2cd92",
+    "url": "/static/media/block_5.c568a00a.jpg"
+  },
+  {
+    "revision": "8f9eb891bb1b2dc6eb178afcc4c1f3f0",
+    "url": "/static/media/block_6.8f9eb891.jpg"
+  },
+  {
+    "revision": "e62aa001c0d0d43a8a1ebdf9adcc83fc",
+    "url": "/static/media/block_7.e62aa001.jpg"
+  },
+  {
+    "revision": "a3134cd725d936f8026fb06f96560d08",
+    "url": "/static/media/block_8.a3134cd7.jpg"
+  },
+  {
+    "revision": "b6f9f4d893660fa8d9944b956c427cec",
+    "url": "/static/media/block_9.b6f9f4d8.jpg"
+  },
+  {
+    "revision": "8f33a34ca09c136c4899decd082f98ff",
+    "url": "/static/media/logo.8f33a34c.jpg"
+  }
+]);
