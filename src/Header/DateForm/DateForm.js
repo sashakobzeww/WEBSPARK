83b8ac5835fe03c243580e2fb6671d760e30ce8f
Alexandr <sashakobzeww@gmail.com>
2019-08-16 11:43:11 +0300

    fx DateForm Добавлено основне содержимое кода
    Но есть баг. Когда нажимаешь на инпут, то появляется белый экран.
    Возможно это связано с тем, что модуля react-datepicker нету в
    json-файлах

--- src/Header/DateForm/DateForm.js
+++ src/Header/DateForm/DateForm.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react'
 import './DateForm.css'
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 class DateForm extends Component {
+
+    state = {
+        startDate: new Date()
+    };
+
+    handleSelectOrChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+
     render() {
         return(
             <>
                 <p>Date</p>
+                <DatePicker
+                    dateFormatCalendar={"dd.MM.YYYY"}
+                    dateFormat={"dd.MM"}
+                    minDate={new Date()}
+                    selected={this.state.startDate}
+                    onSelect={this.handleSelectOrChange.bind(this)}
+                    onChange={this.handleSelectOrChange.bind(this)}
+                />
             </>
         )
     }
